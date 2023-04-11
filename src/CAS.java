26a3565d3dea5a11892dbac3a1dc9bf00dbac868
XiLaGuangTouQiang <cw136429000@gmail.com>
2023-04-11 19:55:48 +0800

    reset

--- src/CAS.java
+++
@@ -1,8 +0,0 @@
-
-import java.util.ArrayList;
-
-public class CAS {
-    public int CASID;
-    public String CASLogic;
-    public ArrayList<LogicName> LogicNameList = new ArrayList<>();
-}
--- src/LogicName.java
+++
@@ -1,6 +0,0 @@
-public class LogicName {
-    public String LogicName;
-    public String ICDName;
-    public String DataType;
-    public String Comment;
-}
